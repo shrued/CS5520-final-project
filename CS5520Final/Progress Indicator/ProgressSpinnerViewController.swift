65ef8baa2203d1086b68cc2ec000c70240d08f3a
HanruC <chen.hanru@northeastern.edu>
2023-12-02 16:02:32 -0500

    recording controller finished, data model added, progress spinner added

---
+++ CS5520Final/Progress Indicator/ProgressSpinnerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProgressSpinnerViewController.swift
+//  CS5520Final
+//
+//  Created by Hanru Chen on 12/2/23.
+//
+
+import UIKit
+
+class ProgressSpinnerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
