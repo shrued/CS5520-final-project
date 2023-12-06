7d8b516ccf6a1c27467d290a2110a48baf12a3e6
HanruC <chen.hanru@northeastern.edu>
2023-12-05 22:37:36 -0500

    overview fixed and controller implemented

--- CS5520Final/Overview Screen/OverviewScreenViewController.swift
+++ CS5520Final/Overview Screen/OverviewScreenViewController.swift
@@ -16,13 +16,21 @@ class OverviewScreenViewController: UIViewController {
     override func loadView() {
         view = overviewScreenView
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fetchUserProfile()
+        fetchDailyIntakeData()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.layoutIfNeeded() // force the layout of subviews before view appears
         overviewScreenView.buttonProfile.addTarget(self, action: #selector(profileButtonTapped), for: .touchUpInside)
         overviewScreenView.buttonUpdate.addTarget(self, action: #selector(saveWeightButtonTapped), for: .touchUpInside)
         
         fetchDailyIntakeData()
+        fetchUserProfile()
     }
     
     @objc func profileButtonTapped() {
@@ -31,24 +39,72 @@ class OverviewScreenViewController: UIViewController {
         navigationController?.pushViewController(profileController, animated: true)
     }
     
+    // save new weight to the firestore database
     @objc func saveWeightButtonTapped() {
-        // validate and save weight info to Firestore
-        // update weight on Profile screen and Overview screen
+        guard let newWeightString = overviewScreenView.textFieldWeightChanges.text,
+              let newWeight = Double(newWeightString) else {
+            presentAlert(title: "Error", message: "Please enter a valid weight")
+            return
+        }
+
+        guard let userEmail = Auth.auth().currentUser?.email else {
+            presentAlert(title: "Error", message: "User not logged in")
+            return
+        }
+
+        // Reference to Firestore
+        let db = Firestore.firestore()
+
+        // Reference to the specific document in the profileInfo subcollection
+        let profileDetailsDocument = db.collection("users").document(userEmail).collection("profileInfo").document("profileDetails")
+
+        // Update the weight in the ProfileInfo document
+        profileDetailsDocument.updateData(["weight": newWeight]) { error in
+            if let error = error {
+                self.presentAlert(title: "Error", message: "Failed to update weight: \(error.localizedDescription)")
+            } else {
+                self.presentAlert(title: "Success", message: "Weight updated successfully")
+                // Optionally, update the UI to reflect the change
+                self.overviewScreenView.setCurrentWeight(newWeight)
+                self.fetchUserProfile()
+            }
+        }
     }
-    
-    // MARK: Fetch daily intake data...
+        
+    func fetchUserProfile() {
+        guard let userEmail = Auth.auth().currentUser?.email else {
+            presentAlert(title: "Error", message: "User not logged in")
+            return
+        }
+        let db = Firestore.firestore()
+        let profileDocument = db.collection("users").document(userEmail).collection("profileInfo").document("profileDetails")
+        
+        profileDocument.getDocument { (document, error) in
+            if let document = document, document.exists {
+                let profileData = try? document.data(as: ProfileInfo.self)
+                if let profileData = profileData {
+                    self.updateProgressbar(currentWeight: profileData.weight, goalWeight: profileData.goalWeight) // MARK: suppose goalweight
+                }
+            } else {
+                self.presentAlert(title: "Error", message: "Document does not exist")
+            }
+        }
+        
+    }
+        
+        // MARK: Fetch daily intake data...
     func fetchDailyIntakeData() {
         guard let userEmail = Auth.auth().currentUser?.email else {
             presentAlert(title: "Error", message: "User not logged in")
             return
         }
-        
-        // get today's date
+            
+            // get today's date
         let today = Date()
         let formatter = DateFormatter() //
         formatter.dateFormat = "yyyy-MM-dd"
         let todayStr = formatter.string(from: today) // "2020-12-23 make it a string"
-        
+            
         // get data from Firestore fetching the collection "users" -> document "userEmail" -> collection "recordingInfo"
         let collectionRecInfo = Firestore.firestore().collection("users").document(userEmail).collection("recordingInfo")
         // get all documents where date = today
@@ -61,8 +117,8 @@ class OverviewScreenViewController: UIViewController {
                 var totalSugar = 0.0
                 var totalSodium = 0.0
                 var totalWater = 0.0
-                
-                // loop through all documents and get the data
+                    
+                    // loop through all documents and get the data
                 for document in querySnapshot!.documents {
                     let data = document.data()
                     totalCalories += data["caloriesAmount"] as? Double ?? 0
@@ -70,38 +126,40 @@ class OverviewScreenViewController: UIViewController {
                     totalSodium += data["sodiumAmount"] as? Double ?? 0
                     totalWater += data["waterAmount"] as? Double ?? 0
                 }
-                // update chart data
+                    // update chart data
                 self.updateIntakeChartData(calories: totalCalories, sugar: totalSugar, sodium: totalSodium, water: totalWater)
             }
         }
     }
-    
-    
-    func updateProgressChart() {
-        // TODO: update progress chart
+        
+        
+    func updateProgressBar(currentWeight: Double, goalWeight: Double) {
+        let progress = (currentWeight - goalWeight) / currentWeight
+        overviewScreenView.weightProgressBar.setProgress(Float(progress), animated: true)
     }
-    
+
+        
     // MARK: Update chart data...
     func updateIntakeChartData(calories: Double, sugar: Double, sodium: Double, water: Double) {
-        
+            
         // update chart data
         let dataSet = BarChartDataSet(entries: [
             BarChartDataEntry(x: 1, y: calories),
             BarChartDataEntry(x: 2, y: sugar),
             BarChartDataEntry(x: 3, y: sodium),
             BarChartDataEntry(x: 4, y: water)
-        ], label: "Daily Intake")
-        
+            ], label: "Daily Intake")
+            
         dataSet.colors = [NSUIColor.systemBlue]
-        
+            
         let data = BarChartData(dataSet: dataSet)
         overviewScreenView.chartViewIntakes.data = data
-        
-        // refresh chart
+            
+            // refresh chart
         overviewScreenView.chartViewIntakes.notifyDataSetChanged()
     }
-    
-    // MARK: Present alert...
+        
+        // MARK: Present alert...
     func presentAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .cancel))
--- CS5520Final/Overview Screen/Views/OverviewScreenView.swift
+++ CS5520Final/Overview Screen/Views/OverviewScreenView.swift
@@ -16,8 +16,9 @@ class OverviewScreenView: UIView {
     var labelCurrentWeightValue: UILabel!
     var textFieldWeightChanges: UITextField!
     var buttonUpdate: UIButton!
-    var chartViewProgress: LineChartView!
     var chartViewIntakes: BarChartView!
+    var weightProgressBar: UIProgressView!
+    var progressBarLabel: UILabel!
 
     var gradientLayer: CAGradientLayer!
     
@@ -30,11 +31,11 @@ class OverviewScreenView: UIView {
         setupLabelCurrentWeight()
         setupTextFieldWeightChanges()
         setupButtonUpdate()
-        setupChartViewProgress()
         setupChartViewIntakes()
         setupLabelCurrentWeightValue()
-        
+        setupWeightProgressBar()
         setupGradientBackground()
+        setupProgressBarLabel()
         initConstraints()
     }
     
@@ -104,7 +105,7 @@ class OverviewScreenView: UIView {
     
     func setCurrentWeight(_ weight: Double) {
         labelCurrentWeightValue.text = "\(weight) lbs"
-        // "\(weight) lbs"
+
     }
     
     // MARK: setup text field weight changes...
@@ -130,13 +131,26 @@ class OverviewScreenView: UIView {
     }
 
     
-    // MARK: setup chart view progress...
-    func setupChartViewProgress() {
-        chartViewProgress = LineChartView()
-        chartViewProgress.noDataText = "No data available"
-        styleChart(chartViewProgress)
-        chartViewProgress.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(chartViewProgress)
+    // MARK: setup bar view for progress
+    func setupWeightProgressBar() {
+        weightProgressBar = UIProgressView(progressViewStyle: .bar)
+        weightProgressBar.trackTintColor = UIColor.lightGray
+        weightProgressBar.progressTintColor = UIColor.systemGreen
+        weightProgressBar.setProgress(0.5, animated: true) // Set this based on user's progress
+        weightProgressBar.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(weightProgressBar)
+    }
+    
+    // MARK: setup label for progress bar
+    func setupProgressBarLabel() {
+        // 2. Initialize the UILabel
+        progressBarLabel = UILabel()
+        progressBarLabel.text = "Progress Bar" // Set the text for the label
+        progressBarLabel.textColor = .black
+        progressBarLabel.textAlignment = .center // Center the text
+        progressBarLabel.font = UIFont.systemFont(ofSize: 16) // Set the font size
+        progressBarLabel.translatesAutoresizingMaskIntoConstraints = false // Enable Auto Layout
+        self.addSubview(progressBarLabel)
     }
     
     // MARK: setup stack view intake labels...
@@ -167,30 +181,35 @@ class OverviewScreenView: UIView {
             buttonProfile.topAnchor.constraint(equalTo: labelOverview.bottomAnchor, constant: 10),
             
             labelCurrentWeight.topAnchor.constraint(equalTo: buttonProfile.bottomAnchor, constant: 20),
-            labelCurrentWeight.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            labelCurrentWeight.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 20),
             
             labelCurrentWeightValue.centerYAnchor.constraint(equalTo: labelCurrentWeight.centerYAnchor),
             labelCurrentWeightValue.leadingAnchor.constraint(greaterThanOrEqualTo: labelCurrentWeight.trailingAnchor, constant: 10),
-            labelCurrentWeightValue.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            labelCurrentWeightValue.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -20),
             
             textFieldWeightChanges.topAnchor.constraint(equalTo: labelCurrentWeight.bottomAnchor, constant: 10),
-            textFieldWeightChanges.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            textFieldWeightChanges.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 20),
             textFieldWeightChanges.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.6),
             
             buttonUpdate.centerYAnchor.constraint(equalTo: textFieldWeightChanges.centerYAnchor),
             buttonUpdate.leadingAnchor.constraint(equalTo: textFieldWeightChanges.trailingAnchor, constant: 10),
-            buttonUpdate.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            buttonUpdate.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -20),
             buttonUpdate.heightAnchor.constraint(equalToConstant: 40),
             
-            chartViewProgress.topAnchor.constraint(equalTo: textFieldWeightChanges.bottomAnchor, constant: 20),
-            chartViewProgress.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
-            chartViewProgress.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
-            chartViewProgress.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.3),
+            weightProgressBar.topAnchor.constraint(equalTo: buttonUpdate.bottomAnchor, constant: 70),
+            weightProgressBar.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            weightProgressBar.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+
+            progressBarLabel.topAnchor.constraint(equalTo: weightProgressBar.bottomAnchor, constant: 8),
+            progressBarLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            progressBarLabel.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            progressBarLabel.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -20),
             
-            chartViewIntakes.topAnchor.constraint(equalTo: chartViewProgress.bottomAnchor, constant: 20),
-            chartViewIntakes.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
-            chartViewIntakes.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
-            chartViewIntakes.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.2)
+
+            chartViewIntakes.topAnchor.constraint(equalTo: progressBarLabel.bottomAnchor, constant: 80),
+            chartViewIntakes.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            chartViewIntakes.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            chartViewIntakes.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.2),
         ])
     }
     
